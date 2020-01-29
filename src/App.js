2e35127eafcf2ef2f88bfe701c8c901e878178bd
Alexjzhh <wangyang.tx@gmail.com>
2020-01-28 23:49:07 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import _ from "lodash";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { validSolution: false };
+
+    this.other = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    this.row_format = [...props.sudoku_data];
+    this.col_format = _.zip(...props.sudoku_data);
+    this.grid_format = this.splitIntoGrid(props.sudoku_data);
+  }
+  splitIntoGrid = sd => {
+    let result = [[], [], [], [], [], [], [], [], []];
+    let r, c;
+    for (r = 0; r < 9; r++) {
+      for (c = 0; c < 9; c++) {
+        // Calculate grid identifiers
+        var gridRow = Math.floor(r / 3);
+        var gridCol = Math.floor(c / 3);
+        var gridIndex = gridRow * 3 + gridCol;
+
+        // Save each grid in a new row
+        result[gridIndex].push(sd[r][c]);
+      }
+    }
+    return result;
+  };
+  validate = segment => {
+    return _.isEqual(segment, this.other);
+  };
+  componentDidMount() {
+    let i;
+    let line_result;
+    let final_result = true;
+    //Exam Row
+    for (i = 0; i < 9; i++) {
+      line_result = this.validate(this.row_format[i].sort());
+      console.log("ROW:", i, line_result);
+      final_result = final_result && line_result;
+    }
+    //Exam Col
+    for (i = 0; i < 9; i++) {
+      line_result = this.validate(this.col_format[i].sort());
+      console.log("COL:", i, line_result);
+      final_result = final_result && line_result;
+    }
+    //Exam Grid
+    for (i = 0; i < 9; i++) {
+      console.log("Grid:", i, this.validate(this.grid_format[i].sort()));
+      final_result = final_result && line_result;
+    }
+    console.log(final_result);
+    this.setState({
+      validSolution: final_result
+    });
+  }
+  render() {
+    console.log(this.state.validSolution);
+    let message = "";
+    if (this.state.validSolution) {
+      message = "Valid Result";
+    } else {
+      message = "Invalid Result";
+    }
+
+    return (
+      <div>
+        App
+        <br />
+        {message}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import App from './app'
+const sudoku_data = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]
+  ];
+ReactDOM.render(<App sudoku_data={sudoku_data}/>, document.querySelector('#root'))
